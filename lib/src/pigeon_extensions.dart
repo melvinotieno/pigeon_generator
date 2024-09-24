f3684121029c51709ca7ef6280db71724292024e
Melvin Otieno <o.melvinotieno@gmail.com>
2024-09-24 15:31:04 +0300

    refactor: remove PigeonExtensions.loadConfig to load pigeon config from BuilderOptions

--- lib/src/pigeon_extensions.dart
+++ lib/src/pigeon_extensions.dart
@@ -1,79 +1,24 @@
-import 'dart:io';
-
-import 'package:checked_yaml/checked_yaml.dart';
-import 'package:path/path.dart' as path;
 import 'package:pigeon/pigeon.dart';
 
-import 'pigeon_config.dart';
-import 'pigeon_validator.dart';
-
-extension PigeonExtension on Pigeon {
-  /// Load the pigeon configuration from the `pigeon.yml` or `pigeon.yaml` file.
-  ///
-  /// If neither of the files exist, a default configuration is provided.
-  static PigeonConfig loadConfig(
-    Map<String, bool?> platformOutputs, {
-    bool? ast,
-    bool? dartTest,
-    bool? debugGenerators,
-  }) {
-    final ymlFile = File('pigeon.yml');
-    final yamlFile = File('pigeon.yaml');
-
-    bool ymlExists = ymlFile.existsSync();
-    bool yamlExists = yamlFile.existsSync();
-
-    if (ymlExists && yamlExists) {
-      throw FileSystemException(
-        'Both pigeon.yml and pigeon.yaml exist. Please remove one of them.',
-        ymlFile.parent.path,
-      );
-    }
-
-    final file = ymlExists ? ymlFile : (yamlExists ? yamlFile : null);
-
-    PigeonConfig pigeonConfig;
-    if (file != null) {
-      pigeonConfig = checkedYamlDecode<PigeonConfig>(
-        file.readAsStringSync(),
-        (map) => PigeonConfig.fromMap(Map.from(map!)),
-        sourceUrl: file.uri,
-      );
-
-      PigeonValidator.validate(pigeonConfig);
-    } else {
-      final cpp = platformOutputs['cpp'];
-      final gobject = platformOutputs['gobject'];
-      final kotlin = platformOutputs['kotlin'];
-      final java = platformOutputs['java'];
-      final swift = platformOutputs['swift'];
-      final objc = platformOutputs['objc'];
-
-      pigeonConfig = PigeonConfig(
-        dart: PigeonDartConfig.defaults(dartTest),
-        cpp: cpp == true ? PigeonCppConfig.defaults() : null,
-        gobject: gobject == true ? PigeonGobjectConfig.defaults() : null,
-        kotlin: kotlin == true ? PigeonKotlinConfig.defaults() : null,
-        java: java == true ? PigeonJavaConfig.defaults() : null,
-        swift: swift == true ? PigeonSwiftConfig.defaults() : null,
-        objc: objc == true ? PigeonObjcConfig.defaults() : null,
-        ast: ast == true ? PigeonAstConfig(out: 'output') : null,
-        debugGenerators: debugGenerators,
-      );
-    }
-
-    if (pigeonConfig.copyrightHeader == null) {
-      pigeonConfig.setCopyrightHeader(
-        path.join(pigeonConfig.inputs, 'copyright.txt'),
-      );
-    }
-
-    return pigeonConfig;
-  }
-}
-
+/// Extension on [PigeonOptions] to provide additional functionality.
 extension PigeonOptionsExtension on PigeonOptions {
-  /// Get the list of outputs based on the pigeon options.
+  /// Gets the list of output file paths based on the current [PigeonOptions].
+  ///
+  /// This method collects all the output file paths specified in the
+  /// [PigeonOptions] for various languages and configurations.
+  ///
+  /// It includes paths for:
+  /// - Dart (main and test)
+  /// - C++ (header and source)
+  /// - GObject (header and source)
+  /// - Kotlin
+  /// - Java
+  /// - Swift
+  /// - Objective-C (header and source)
+  /// - AST (Abstract Syntax Tree)
+  ///
+  /// Returns:
+  ///   A [List<String>] containing all non-null output file paths.
   List<String> getOutputs() {
     final outputs = [
       dartOut,
