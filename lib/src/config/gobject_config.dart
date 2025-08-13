c3ccef2a7ce8b175e175f6556629123db546860d
Melvin Otieno <o.melvinotieno@gmail.com>
2025-08-13 08:02:33 +0300

    refactor: remove inheritance from BaseConfig and simplify GObjectConfig structure

--- lib/src/config/gobject_config.dart
+++ lib/src/config/gobject_config.dart
@@ -1,18 +1,38 @@
-import 'package:pigeon/pigeon.dart';
+import 'dart:io';
 
-import 'base_config.dart';
 import 'output_config.dart';
 
-final class GObjectConfig extends BaseConfig {
-  GObjectConfig._internal({this.headerOut, this.sourceOut, this.options});
+/// Configuration for the GObject code generation.
+class GObjectConfig {
+  GObjectConfig._internal({this.headerOut, this.sourceOut});
 
+  /// Configuration for the ".h" GObject file that will be generated.
   final OutputConfig? headerOut;
 
+  /// Configuration for the ".cc" GObject file that will be generated.
   final OutputConfig? sourceOut;
 
-  final GObjectOptions? options;
+  /// Creates a [GObjectConfig] instance from a map.
+  ///
+  /// If the map is `false`, or `null` and the "linux" directory does not
+  /// exist, it returns a [GObjectConfig] with null values. Otherwise, it
+  /// returns a [GObjectConfig] with default values.
+  factory GObjectConfig.fromMap(dynamic map) {
+    if (map == false || (map == null && !Directory('linux').existsSync())) {
+      return GObjectConfig._internal();
+    }
 
-  static GObjectConfig? fromMap(Map<String, dynamic>? map) {
-    return null;
+    map = map is Map ? map : <String, dynamic>{};
+
+    return GObjectConfig._internal(
+      headerOut: OutputConfig.fromOptions(
+        map['header_out'] as String? ?? 'linux',
+        extension: 'h',
+      ),
+      sourceOut: OutputConfig.fromOptions(
+        map['source_out'] as String? ?? 'linux',
+        extension: 'cc',
+      ),
+    );
   }
 }
