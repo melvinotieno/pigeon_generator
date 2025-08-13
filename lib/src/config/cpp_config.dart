c429431e26391dfb39d00d1a24d5890c3ecb367e
Melvin Otieno <o.melvinotieno@gmail.com>
2025-08-13 07:58:30 +0300

    refactor: simplify CppConfig class and remove inheritance from BaseConfig

--- lib/src/config/cpp_config.dart
+++ lib/src/config/cpp_config.dart
@@ -1,19 +1,38 @@
-import 'package:pigeon/pigeon.dart';
+import 'dart:io';
 
-import 'base_config.dart';
 import 'output_config.dart';
 
-final class CppConfig extends BaseConfig {
-  CppConfig._internal({this.headerOut, this.sourceOut, this.options});
+/// Configuration for C++ code generation.
+class CppConfig {
+  CppConfig._internal({this.headerOut, this.sourceOut});
 
+  /// Configuration for the ".h" C++ file that will be generated.
   final OutputConfig? headerOut;
 
+  /// Configuration for the ".cpp" C++ file that will be generated.
   final OutputConfig? sourceOut;
 
-  final CppOptions? options;
+  /// Creates a [CppConfig] instance from a map.
+  ///
+  /// If the map is `false`, or `null` and the "windows" directory does not
+  /// exist, it returns a [CppConfig] with null values. Otherwise, it returns
+  /// a [CppConfig] with default values.
+  factory CppConfig.fromMap(dynamic map) {
+    if (map == false || (map == null && !Directory('windows').existsSync())) {
+      return CppConfig._internal();
+    }
 
-  static CppConfig? fromMap(dynamic map) {
-    if (map == null) return null;
-    return null;
+    map = map is Map ? map : <String, dynamic>{};
+
+    return CppConfig._internal(
+      headerOut: OutputConfig.fromOptions(
+        map['header_out'] as String? ?? 'windows/runner',
+        extension: 'h',
+      ),
+      sourceOut: OutputConfig.fromOptions(
+        map['source_out'] as String? ?? 'windows/runner',
+        extension: 'cpp',
+      ),
+    );
   }
 }
