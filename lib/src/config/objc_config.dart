b1f8a7d0957708e1c3c13e5322aa6c11d220604e
Melvin Otieno <o.melvinotieno@gmail.com>
2025-08-16 00:14:33 +0300

    refactor: simplify ObjcConfig class by removing inheritance and unused options

--- lib/src/config/objc_config.dart
+++ lib/src/config/objc_config.dart
@@ -1,18 +1,38 @@
-import 'package:pigeon/pigeon.dart';
+import 'dart:io';
 
-import 'base_config.dart';
 import 'output_config.dart';
 
-final class ObjcConfig extends BaseConfig {
-  ObjcConfig._internal({this.headerOut, this.sourceOut, this.options});
+/// Configuration for Objective-C code generation.
+final class ObjcConfig {
+  ObjcConfig._internal({this.headerOut, this.sourceOut});
 
+  /// Configuration for the ".h" Objective-C file that will be generated.
   final OutputConfig? headerOut;
 
+  /// Configuration for the ".m" Objective-C file that will be generated.
   final OutputConfig? sourceOut;
 
-  final ObjcOptions? options;
+  /// Creates a [ObjcConfig] instance from a map.
+  ///
+  /// If the map is `false`, or `null` and the "macos" directory does not
+  /// exist, it returns a [ObjcConfig] with null values. Otherwise, it
+  /// returns a [ObjcConfig] with default values.
+  factory ObjcConfig.fromMap(dynamic map) {
+    if (map == false || (map == null && !Directory('macos').existsSync())) {
+      return ObjcConfig._internal();
+    }
 
-  static ObjcConfig? fromMap(Map<String, dynamic>? map) {
-    return null;
+    map = map is Map ? map : <String, dynamic>{};
+
+    return ObjcConfig._internal(
+      headerOut: OutputConfig.fromOptions(
+        map['header_out'] as String? ?? 'macos',
+        extension: 'h',
+      ),
+      sourceOut: OutputConfig.fromOptions(
+        map['source_out'] as String? ?? 'macos',
+        extension: 'm',
+      ),
+    );
   }
 }
