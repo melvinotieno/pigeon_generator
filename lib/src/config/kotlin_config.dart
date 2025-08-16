f28976bb764dec2df6167b6aaf4de438d2225388
Melvin Otieno <o.melvinotieno@gmail.com>
2025-08-16 15:15:06 +0300

    refactor: remove inheritance from BaseConfig and simplify KotlinConfig implementation

--- lib/src/config/kotlin_config.dart
+++ lib/src/config/kotlin_config.dart
@@ -1,16 +1,56 @@
+import 'dart:io';
+
 import 'package:pigeon/pigeon.dart';
 
-import 'base_config.dart';
+import '../utilities/android.dart';
 import 'output_config.dart';
 
-final class KotlinConfig extends BaseConfig {
-  KotlinConfig._internal({this.out, this.options});
+/// Configuration for Kotlin code generation.
+class KotlinConfig {
+  KotlinConfig._internal({this.out, KotlinOptions? options})
+    : _options = options;
 
+  /// Configuration for the ".kt" file that will be generated.
   final OutputConfig? out;
 
-  final KotlinOptions? options;
+  /// Options that control how Kotlin will be generated.
+  final KotlinOptions? _options;
 
-  static KotlinConfig? fromMap(Map<String, dynamic>? map) {
-    return null;
+  /// Creates a [KotlinConfig] instance from a map.
+  ///
+  /// If the map is `false`, or `null` and the "android" directory does not exist,
+  /// it returns a [KotlinConfig] with null values. Otherwise, it returns a
+  /// [KotlinConfig] with default values.
+  factory KotlinConfig.fromMap(dynamic map) {
+    if (map == false || (map == null && !Directory('android').existsSync())) {
+      return KotlinConfig._internal();
+    }
+
+    map = map is Map ? map : <String, dynamic>{};
+
+    final options = map['options'] as Map<String, dynamic>? ?? {};
+    final android = Android().get('kotlin', map['out'], options['package']);
+
+    return KotlinConfig._internal(
+      out: OutputConfig.fromOptions(
+        android['outPath'],
+        extension: 'kt',
+        pascalCase: true,
+      ),
+      options: KotlinOptions(
+        package: android['packageName'],
+        copyrightHeader: options['copyright_header'] as Iterable<String>?,
+        includeErrorClass: options['include_error_class'] as bool? ?? true,
+      ),
+    );
+  }
+
+  /// Returns the Kotlin options for the given file name.
+  KotlinOptions? getOptions(String fileName) {
+    if (_options == null) return null;
+
+    return _options.merge(
+      KotlinOptions(errorClassName: '${fileName.pascalCase}Error'),
+    );
   }
 }
