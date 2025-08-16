bbfcc0f14e9bb6ac875e12e4d15815174a73ad16
Melvin Otieno <o.melvinotieno@gmail.com>
2025-08-16 15:14:49 +0300

    refactor: remove inheritance from BaseConfig and streamline SwiftConfig implementation

--- lib/src/config/swift_config.dart
+++ lib/src/config/swift_config.dart
@@ -1,16 +1,50 @@
+import 'dart:io';
+
 import 'package:pigeon/pigeon.dart';
 
-import 'base_config.dart';
 import 'output_config.dart';
 
-final class SwiftConfig extends BaseConfig {
-  SwiftConfig._internal({this.out, this.options});
+/// Configuration for Swift code generation.
+class SwiftConfig {
+  SwiftConfig._internal({this.out, Map<String, dynamic>? options})
+    : _options = options;
 
+  /// Configuration for the ".swift" file that will be generated.
   final OutputConfig? out;
 
-  final SwiftOptions? options;
+  /// Options that control how Swift will be generated.
+  final Map<String, dynamic>? _options;
 
-  static SwiftConfig? fromMap(Map<String, dynamic>? map) {
-    return null;
+  /// Creates a [SwiftConfig] instance from a map.
+  ///
+  /// If the map is `false`, or `null` and the "ios" directory does not exist,
+  /// it returns a [SwiftConfig] with null values. Otherwise, it returns a
+  /// [SwiftConfig] with default values.
+  factory SwiftConfig.fromMap(dynamic map) {
+    if (map == false || (map == null && !Directory('ios').existsSync())) {
+      return SwiftConfig._internal();
+    }
+
+    map = map is Map ? map : <String, dynamic>{};
+
+    return SwiftConfig._internal(
+      out: OutputConfig.fromOptions(
+        map['out'] as String? ?? 'ios',
+        extension: 'swift',
+        pascalCase: true,
+      ),
+      options: map['options'] as Map<String, dynamic>?,
+    );
+  }
+
+  /// Returns the Swift options for the given file name.
+  SwiftOptions? getOptions(String fileName) {
+    if (_options == null) return null;
+
+    return SwiftOptions(
+      copyrightHeader: _options['copyright_header'] as Iterable<String>?,
+      includeErrorClass: _options['include_error_class'] as bool? ?? true,
+      errorClassName: '${fileName.pascalCase}Error',
+    );
   }
 }
